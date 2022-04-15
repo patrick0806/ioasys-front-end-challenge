7f42ea3a7c1c92c8070f18fff802ae4c2e4ee4f1
Patrick da Silva Nicezi <patrickk0806@hotmail.com>
2022-04-14 21:38:18 -0300

    grid works

--- src/pages/Books/Books.styles.ts
+++ src/pages/Books/Books.styles.ts
@@ -1,20 +1,52 @@
 import styled from "styled-components";
 import background from "../../assets/images/background.png";
 
-/* export const Container = styled.div`
+export const PageTitle = styled.h1`
+  color: #333333;
+  font-size: 28px;
+  display: flex;
+  font-weight: 300;
+  align-items: center;
+  height: 30px;
+
+  @media (max-width: 768px) {
+    width: 295px;
+  }
+`;
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
   background: url(${background});
   background-size: cover;
+  overflow-x: hidden;
+`;
+
+export const Menu = styled.div`
+  
+  height: 200px;
   display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0;
+`;
+
+export const UserMenu = styled.div`
+  display: flex;
+  flex-direction: row;
   align-items: center;
   justify-content: center;
 `;
 
 export const BooksContainer = styled.div`
+  
   display: grid;
-  grid-template-columns: 25% 25% 25% 25%;
+  grid-template-columns: repeat(4, auto);
   grid-template-rows: 135px 135px 135px;
   grid-gap: 15px;
-  background-color: black;
   height: 500px;
   @media (max-width: 768px) {
     display: flex;
@@ -23,48 +55,10 @@ export const BooksContainer = styled.div`
   }
 `;
 
-export const Content = styled.div`
-    width: 90%;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-
-    background-color: green;
-`
-
-export const PageTitle = styled.h1`
-  width: 100%;
-  color: #333333;
-  font-size: 28px;
+export const User = styled.div`
+  padding-right: 15px;
   display: flex;
-  font-weight: 300;
+  flex-direction: row;
   align-items: center;
-  height: 30px;
-  margin-bottom: 50px;
-  @media (max-width: 768px) {
-    width: 295px;
-    margin-bottom: 20px;
-  }
-  &svg {
-    padding-right: 5px;
-  }
-`; */
-
-
-export const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    background-color: red;
-`
-
-export const Menu = styled.div`
-background-color: green;
-width: 85%;
-height: 200px;
-`
-
-export const BooksContainer = styled.div`
-    background-color: yellow;
-    width: 85%;
-`
+  justify-content: center;
+`;
--- src/pages/Books/Books.tsx
+++ src/pages/Books/Books.tsx
@@ -3,7 +3,9 @@ import * as S from "./Books.styles";
 import { Book, BookList } from "../../types/Book";
 import { BookCard } from "../../components/BookCard/BookCard";
 import { getBooks } from "../../services/getBooks";
-import { Logo } from "../../assets/icons";
+import { Exit, Logo } from "../../assets/icons";
+
+const user = "Patrick";
 
 export function BooksPage() {
   const [bookList, setBookList] = React.useState<BookList>();
@@ -19,23 +21,21 @@ export function BooksPage() {
 
   return (
     <S.Container>
-      <S.Menu>menu</S.Menu>
+      <S.Menu>
+        <S.PageTitle>
+          <Logo color={"#333333"} /> &nbsp; Books
+        </S.PageTitle>
+        <S.UserMenu>
+          <S.User>Bem vindo, {user}</S.User>
+          <Exit onClick={()=>console.log('cliquei')}/>
+        </S.UserMenu>
+      </S.Menu>
       <S.BooksContainer>
-        book
+        {bookList &&
+          bookList.data.map((book: Book) => {
+            return <BookCard key={book.id} book={book} />;
+          })}
       </S.BooksContainer>
     </S.Container>
   );
 }
-/* <S.Container>
-      <S.Content>
-        <S.PageTitle>
-          <Logo color={"#333333"}/> &nbsp; Books
-        </S.PageTitle>
-        <S.BooksContainer>
-          {bookList &&
-            bookList.data.map((book: Book) => {
-              return <BookCard key={book.id} book={book} />;
-            })}
-        </S.BooksContainer>
-      </S.Content>
-    </S.Container> */
